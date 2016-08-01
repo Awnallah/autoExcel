d5b7b71983af2d0a407fa33160f6899188904792
Awnallah <6hamzah6@gmail.com>
2016-08-01 09:14:17 -0700

    fetched table main headers

--- autoGen.py
+++ autoGen.py
@@ -1,9 +1,8 @@
 from openpyxl import load_workbook, Workbook
 import string, itertools
 
-# letters= string.ascii_uppercase
-# letters_dic = {i:let for i, let in enumerate(letters, start=0)}
-# print letters_dic
+letters= string.ascii_uppercase
+
 
 
 wb = load_workbook(filename='data.xlsx')
@@ -14,12 +13,13 @@ sheet1 =wb['Sheet1']
 
 projects=[]
 
+#indeces of headers
 headers= [x.value.encode('UTF8') for x in sheet1.rows[0]]
 
 project_index = headers.index('PROJECT')
 transfer_index = headers.index('TRANS_AMT')
 
-
+# make a headers dictionary
 headers_dic = {v:k for k,v in enumerate(headers, start=0)}
 print headers_dic['PROJECT']
 
@@ -36,26 +36,45 @@ offset= 5
 
 projects_dic = {v:k for (k,v) in enumerate(projects, start=offset)}
 
+#dictionary for each table header to new excel column letter
+first_row_headers = ['DOC_NO','DOC_SUFFIX', 'Fund','FISCAL_YR','FISCAL_MO','Post Date']
+letters_dic = {let:header_name for let, header_name in zip(first_row_headers,letters)}
+print letters_dic
+
+first_row = first_row_headers + projects
 
 
 new_book = Workbook()
 new_sheet = new_book.active
-new_sheet.append(projects)
+#new_sheet.append(projects)
+
+
+new_sheet.append(first_row)
 project_col =[]
 for col in new_sheet.rows[0]:
+	if col.value not in projects:
+		continue
 	project_col.append(col.column)
 
 project_col_dic = {proj:let for proj,let in zip(projects,project_col)}
-#print project_col_dic
+print project_col_dic
+
+print headers_dic
 
 for row in sheet1.rows[1:]:
-	current_proj=headers_dic['PROJECT']
-	current_amt =headers_dic['TRANS_AMT']
-	#if row[current_col].value and row[current_col].value != "PROJECT":
-	cc = project_col_dic[row[current_proj].value]
-	rr = row[current_proj].row
-	location = cc+str(rr)
-	new_sheet[location]= row[current_amt].value
+	current_proj = headers_dic['PROJECT']
+	current_amt = headers_dic['TRANS_AMT']
+	current_doc = headers_dic['DOC_NO']
+	current_suffix = headers_dic['DOC_SUFFIX']
 	
+	c = project_col_dic[row[current_proj].value]
+	r = row[current_proj].row
+	location = c+str(r)
+	new_sheet[letters_dic['DOC_NO'] +str(r)] = row[current_doc].value
+	new_sheet[letters_dic['DOC_SUFFIX'] +str(r)] = row[current_suffix].value
+	new_sheet[location]= row[current_amt].value
+
+	
+
 
 new_book.save('testing.xlsx')
